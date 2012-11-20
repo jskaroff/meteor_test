b412d0e94c32bc63aa30acf25bea9b62ef7be00c
Josh Skaroff <josh.skaroff@stuzo.com>
2012-11-20 13:21:18 -0500

    It's all broken

--- meteor_test.js
+++ meteor_test.js
@@ -8,12 +8,34 @@ if (Meteor.isClient) {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
+        alert("Button press");
+    }
+  });
+
+  Template.facebook.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      console.log("Click FB login");
+      Meteor.loginWithFacebook(['user_likes'], 
+      function (err) {
+        if (err)
+          console.log(err.reason);
+          //Session.set('errorMessage', err.reason || 'Unknown error');
+      });
     }
   });
 }
 
+
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+
+    Accounts.ui.config({
+      requestPermissions: {
+        facebook: ['user_likes']
+      },
+      passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+    });
   });
 }
